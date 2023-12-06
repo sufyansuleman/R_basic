2e6aa3e4f4a59401be7d5af8808b51dc9927e68e
Sufyan Suleman <rnh585@ku.dk>
2023-12-06 15:57:17 +0100

    made into book format

---
+++ R/load-packages.R
@@ -0,0 +1,9 @@
+# This script is calling the package libraries, all the packages involved will then be added to this script.
+# Source this file before each session to load the packages.
+
+library(here)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(usethis)
